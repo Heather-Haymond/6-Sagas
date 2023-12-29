f0c1a56a438ac666e1057c3e752c85a7f4bc2f2c
Marc McCarthy <marstheory20@gmail.com>
2023-12-28 19:06:31 -0600

    added api to routes and testing routes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const zooRouter = require('./routes/zoo.router.js');
 app.use(express.json());
 app.use(express.static('build'));
 
-app.use('/zoo', zooRouter);
+app.use('/api/zoo', zooRouter);
 
 module.exports = app.listen(PORT, function () {
     console.log('Listening on port: ', PORT);
