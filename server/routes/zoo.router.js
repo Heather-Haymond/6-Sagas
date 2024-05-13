b5a9ebf0e0b3daff40fdea0d6fe3174c0cbafc41
Heather Haymond <hheatherjean11@gmail.com>
2024-05-13 12:22:05 -0500

    trying

--- server/routes/zoo.router.js
+++ server/routes/zoo.router.js
@@ -5,8 +5,58 @@ const pool = require('../modules/pool.js');
 
 router.get('/', (req, res) => {
     // YOUR CODE HERE
-
+    const query = `
+    SELECT * FROM "class"
+      ORDER BY "class_name" ASC;
+  `;
+  pool.query(query)
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get class', err);
+      res.sendStatus(500)
+    })
 });
 
 
-module.exports = router;
+router.get("/:id", (req, res) => {
+    const query = `
+      SELECT 
+          species.species_name 
+      FROM 
+          species
+      JOIN 
+          species_name ON species.id = species_name.species_id
+      JOIN 
+          class ON class.id = class_name.class_id
+      WHERE 
+          class.id = $1;
+    `;
+    pool
+      .query(query, [req.params.id])
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((err) => {
+        console.error("Error fetching genres:", err);
+        res.sendStatus(500);
+      });
+  });
+   
+// module.exports = router;
+// The previous developer got stuck and couldn't figure out:
+// 1. How to write a SQL join.
+// 2. How to implement the `redux-saga` library.
+
+// Everything else in this app is bug-free and finished, including automated tests! Thanks to this, **you'll only need to write code inside two files**:
+// 1. `zoo.router.js`
+// 2. `store.js`
+
+// There's also a `database.sql` file that you'll use to construct a database with two tables:
+// 1. `species`
+// 2. `class`
+
+// All that's left for you to do is:
+// 1. Create a `GET` route that responds with all of the zoo animal data from the database.
+// 2. Make a Saga function that'll fetch the zoo animal data and put it into the `zooAnimals` reducer.
--- src/store.js
+++ src/store.js
@@ -1,17 +1,33 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import {takeLatest, put} from 'redux-saga/effects'
 import axios from "axios";
 
 
 // Your saga should listen for the action type of `GET_ZOO_ANIMALS`
 function* rootSaga() {
   // Your code here.
-}
+  yield takeLatest('GET_ZOO_ANIMALS', getZooAnimals);
 
+}
 
 // This reducer is waiting for you to put an array of
 // zoo animal objects in it...
+function* getZooAnimals () {
+    console.log('In the Zoo')
+
+    try { //(get function)
+      let response = yield axios.get('/api/zoo');
+        yield put({
+            type: 'SET_ZOO_ANIMALS',
+            payload: response.data });
+    } catch (error){
+        console.log('in get animals error:', error);
+    }
+    
+     //put in saga is the same as dispatch in react
+    }
 const zooAnimals = (state = [], action) => {
     switch (action.type) {
         case 'SET_ZOO_ANIMALS':
@@ -20,6 +36,25 @@ const zooAnimals = (state = [], action) => {
             return state;
     }
 }
+const species = (state = [], action) => {
+   
+    switch (action.type) {
+        case 'SET_SPECIES':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+// const animalClass = (state = [], action) => {
+   
+//     switch (action.type) {
+//         case 'SET_ANIMAL_CLASS':
+//             return action.payload;
+//         default:
+//             return state;
+//     }
+// }
 
 // Create the sagaMiddleware:
 const sagaMiddleware = createSagaMiddleware();
@@ -31,6 +66,8 @@ export const store = () => {
     const store = createStore(
         combineReducers({
             zooAnimals,
+            species
+            
         }),
         // Add sagaMiddleware and redux logger to our store:
         applyMiddleware(sagaMiddleware, logger),
@@ -42,3 +79,21 @@ export const store = () => {
     // Return the redux store:
     return store;
 }
+
+
+// module.exports = router;
+// The previous developer got stuck and couldn't figure out:
+// 1. How to write a SQL join.
+// 2. How to implement the `redux-saga` library.
+
+// Everything else in this app is bug-free and finished, including automated tests! Thanks to this, **you'll only need to write code inside two files**:
+// 1. `zoo.router.js`
+// 2. `store.js`
+
+// There's also a `database.sql` file that you'll use to construct a database with two tables:
+// 1. `species`
+// 2. `class`
+
+// All that's left for you to do is:
+// 1. Create a `GET` route that responds with all of the zoo animal data from the database.
+// 2. Make a Saga function that'll fetch the zoo animal data and put it into the `zooAnimals` reducer.
